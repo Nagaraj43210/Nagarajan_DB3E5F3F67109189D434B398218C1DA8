a8f6d0ad993a6b479459d3f312003f8eaeadba97
Nagaraj43210 <144437916+Nagaraj43210@users.noreply.github.com>
2023-10-26 11:21:45 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,21 +1,13 @@
-# Python program to check if year is a leap year or not
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+          
+    return indices 
+  
+products = ['shoes','boot','loafer','shoes','sandal','shoes']
 
-year = 2000
-
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
-
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+target_product = 'shoes'
+result = linear_search_product(products, target_product)
+print(result)
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-# Python program to find the factorial of a number provided by the user.
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
 
-# change the value for a different result
-num = 7
+# Define a student class
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-# To take input from the user
-#num = int(input("Enter a number: "))
+# Test the function with sample student objects
+student1 = Student("Alice", "S001", 3.8)
+student2 = Student("Bob", "S002", 3.9)
+student3 = Student("Charlie", "S003", 3.7)
 
-factorial = 1
+students = [student1, student2, student3]
 
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
